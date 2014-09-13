14e37f90e2ea7e46c5fe20f217456e03cafc9661
GS <oracle304-oracle@yahoo.co.uk>
2014-09-13 21:16:50 +0100

    TIDY UP MISSING NEW LINES. REFORMAT FEATURE LIST

--- pdb.sh
+++ pdb.sh
@@ -9,17 +9,20 @@ STG_PDB=$(<~/.pdbstg)
 PRD_PDB=$(<~/.pdbprd)
 TMP1=/var/tmp/$(basename $0).tmp1
 TMP2=/var/tmp/$(basename $0).tmp2
+
 function curl_nodes {
 curl -s $STG_PDB/nodes/pending?per_page=all >  $TMP1
 curl -s $PRD_PDB/nodes/pending?per_page=all >  $TMP2
 }
+
 curl_nodes
+
 printf  "\n Total Stg nodes: "
 awk 'c&&!--c;/class=.all/{c=2}' $TMP1 |cut -f2 -d'>' |cut -f1 -d'<'
 printf  " Total Prd nodes: "
 awk 'c&&!--c;/class=.all/{c=2}' $TMP2 |cut -f2 -d'>' |cut -f1 -d'<'
 
-printf "\n Display pending nodes in Stg1, Stg2 or Prd\n"
+printf "\n Display pending nodes in Stg1, Stg2 or Prd?\n"
 read -p $' Enter  1  2 or p: ' FILTER
 printf "\n =================\n\n"
 
